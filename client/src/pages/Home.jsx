a56625c3ea8826794bbcabca8a4f927914fa6196
SSSkeletry <134796582+SSSkeletry@users.noreply.github.com>
2025-03-18 01:05:29 +0200

    Update package.json, improve Home page styling, and enhance responsiveness

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,57 +5,56 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Home = () => {
   return (
-    <>
-      <main>
-        <section className="promo">
-          <div className="promo-video">
-            <video
-              src={promo}
-              alt="Оптимізація"
-              className="circle"
-              autoPlay
-              loop
-              muted
-              playsInline
-            />
-          </div>
-          <article>
-            <h1>
-              <span>ГОТОВИЙ</span> ВИВЕСТИ <span>САЙТ</span> НА НОВИЙ{" "}
-              <span>РIВ</span>ЕНЬ?
-            </h1>
-            <p>
-              Ми знаємо, як зробити твій сайт швидшим, видимішим та
-              ефективнішим. Незалежно від того, чи ти запускаєш новий проект або
-              хочеш покращити існуючий сайт – у нас є інструменти та досвід, щоб
-              зробити це можливим.
-            </p>
-            <aside className="promo-stats">
-              <ul>
-                <li>
-                  <span className="num">3+</span>
-                  <span>Років досвіду</span>
-                </li>
-                <li>
-                  <span className="num">200+</span>
-                  <span>Оптимізованих сайтів</span>
-                </li>
-                <li>
-                  <span className="num">100+</span>
-                  <span>Покращенних позицій</span>
-                </li>
-              </ul>
-            </aside>
-            <nav>
-              <button className="btn-custom">
-                <span className="btn-text">Почніть оптимізацію зараз</span>
-                <i className="bi bi-caret-right btn-icon"></i>
-              </button>
-            </nav>
-          </article>
-        </section>
-      </main>
-    </>
+    <main>
+      <section className="promo">
+        <div className="promo-video">
+          <video
+            src={promo}
+            alt="Оптимізація"
+            className="circle"
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload="auto"
+          />
+        </div>
+        <article>
+          <h1>
+            <span>ГОТОВИЙ</span> ВИВЕСТИ <span>САЙТ</span> НА НОВИЙ{" "}
+            <span>РIВ</span>ЕНЬ?
+          </h1>
+          <p>
+            Ми знаємо, як зробити твій сайт швидшим, видимішим та ефективнішим.
+            Незалежно від того, чи ти запускаєш новий проект або хочеш покращити
+            існуючий сайт – у нас є інструменти та досвід, щоб зробити це
+            можливим.
+          </p>
+          <aside className="promo-stats">
+            <ul>
+              <li>
+                <span className="num">3+</span>
+                <span>Років досвіду</span>
+              </li>
+              <li>
+                <span className="num">200+</span>
+                <span>Оптимізованих сайтів</span>
+              </li>
+              <li>
+                <span className="num">100+</span>
+                <span>Покращенних позицій</span>
+              </li>
+            </ul>
+          </aside>
+          <nav>
+            <button className="btn-custom">
+              <span className="btn-text">Почніть оптимізацію зараз</span>
+              <i className="bi bi-caret-right btn-icon"></i>
+            </button>
+          </nav>
+        </article>
+      </section>
+    </main>
   );
 };
 export default Home;
