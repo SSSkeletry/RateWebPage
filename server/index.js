4966b572ba6570fe974f3eab465912bf21083961
SSSkeletry <134796582+SSSkeletry@users.noreply.github.com>
2025-07-15 14:24:08 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-  res.send("API работает!");
+  res.send("API працює!");
 });
 
 app.post("/api/analyze", authenticateTokenOptional, async (req, res) => {
